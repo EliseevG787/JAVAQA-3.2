6b25112cc79233f932c09adfdd77350930c4f445
unknown <rdn787@bk.ru>
2021-02-14 13:57:09 +0300

    added Price.java

--- src/main/java/ru/netology/domain/attachment/Price.java
+++ src/main/java/ru/netology/domain/attachment/Price.java
@@ -1,2 +1,40 @@
-package ru.netology.domain.attachment;public class Price {
+package ru.netology.domain.attachment;
+
+public class Price {
+    private String amount;
+    private Currency currency;
+    private String oldAmount;
+    private String text;
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(Currency currency) {
+        this.currency = currency;
+    }
+
+    public String getOldAmount() {
+        return oldAmount;
+    }
+
+    public void setOldAmount(String oldAmount) {
+        this.oldAmount = oldAmount;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
 }
