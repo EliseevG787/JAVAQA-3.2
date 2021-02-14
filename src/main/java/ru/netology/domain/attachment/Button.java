755bf0e1f50924fb4a0f07da895f16283f3d06b0
unknown <rdn787@bk.ru>
2021-02-14 13:36:56 +0300

    added Button.java

--- src/main/java/ru/netology/domain/attachment/Button.java
+++ src/main/java/ru/netology/domain/attachment/Button.java
@@ -1,2 +1,22 @@
-package ru.netology.domain.attachment;public class Button {
+package ru.netology.domain.attachment;
+
+public class Button {
+    private String title;
+    private Action action;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Action getAction() {
+        return action;
+    }
+
+    public void setAction(Action action) {
+        this.action = action;
+    }
 }
