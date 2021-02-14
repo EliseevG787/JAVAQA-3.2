df0b10c08429742e36165cbb15ea415e643aa48a
unknown <rdn787@bk.ru>
2021-02-14 13:52:00 +0300

    added MarketAlbum.java

--- src/main/java/ru/netology/domain/attachment/MarketAlbum.java
+++ src/main/java/ru/netology/domain/attachment/MarketAlbum.java
@@ -1,2 +1,58 @@
-package ru.netology.domain.attachment;public class MarketAlbum {
+package ru.netology.domain.attachment;
+
+public class MarketAlbum {
+    private int id;
+    private int ownerId;
+    private Section title;
+    private Photo photo;
+    private int count;
+    private int updatedTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(int ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Section getTitle() {
+        return title;
+    }
+
+    public void setTitle(Section title) {
+        this.title = title;
+    }
+
+    public Photo getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(Photo photo) {
+        this.photo = photo;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getUpdatedTime() {
+        return updatedTime;
+    }
+
+    public void setUpdatedTime(int updatedTime) {
+        this.updatedTime = updatedTime;
+    }
 }
